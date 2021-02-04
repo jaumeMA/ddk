e50aae6f086132bbc92f28410e7e465213cc3a40
jaumeMA <capcir@gmail.com>
2021-02-04 12:51:34 +0100

    Added temporary fix for static counter in windows and linux

--- src/ddkFramework/ddk_rtti.h
+++ src/ddkFramework/ddk_rtti.h
@@ -26,7 +26,7 @@ struct static_typed_number
 {
 };
 template<typename T>
-struct static_typed_number<T,-1>
+struct static_typed_number<T,0>
 {
     friend inline mpl::type_pack<> __get_inherited_type_list(const static_typed_number&);
 };
--- src/ddkFramework/ddk_static_counter.h
+++ src/ddkFramework/ddk_static_counter.h
@@ -1,5 +1,16 @@
 #pragma once
 
+#if defined(WIN32)
+
+#define INITIAL_VALUE 1
+
+#else
+
+#define INITIAL_VALUE 0
+
+#endif
+
+
 namespace ddk
 {
 namespace detail
@@ -8,18 +19,18 @@ namespace detail
 template<typename T,size_t N>
 struct counter_tag
 {
-	friend constexpr size_t counter_index(const counter_tag<T,N>&);
+	friend constexpr size_t ___counter_index(const counter_tag<T,N>&);
 };
 
 template<typename T,size_t N>
 struct producer
 {
-	friend constexpr size_t counter_index(const counter_tag<T,N>&)
+	friend constexpr size_t ___counter_index(const counter_tag<T,N>&)
 	{
 		return N;
 	}
 
-	static constexpr size_t value = counter_index(counter_tag<T,N>{});
+	static constexpr size_t value = ___counter_index(counter_tag<T,N>{});
 };
 
 template<typename T,size_t N,size_t = producer<T,N>::value, typename ... TT>
@@ -28,31 +39,31 @@ size_t constexpr consumer(float,const counter_tag<T,N>&,TT...)
 	return N;
 }
 
-template<typename T,size_t N,size_t V = counter_index(counter_tag<T,N>{}), size_t R = consumer<T>(0,counter_tag<T,N + 1> {})>
+template<typename T,size_t N,size_t V = ___counter_index(counter_tag<T,N>{}), size_t R = consumer<T>(0,counter_tag<T,N + 1> {})>
 size_t constexpr consumer(int,const counter_tag<T,N>&)
 {
 	return R;
 }
 
-template<typename T,size_t N, size_t V = 0, typename ... TT>
-size_t constexpr curr_tag(float,const counter_tag<T,N>&,TT...)
+template<typename T,size_t N, size_t V = 100>
+size_t constexpr curr_tag(float,const counter_tag<T,N>&, ...)
 {
 	return N-1;
 }
 
-template<typename T,size_t N,size_t V = counter_index(counter_tag<T,N>{}), size_t R  = curr_tag<T>(0,counter_tag<T,N + 1> {})>
+template<typename T,size_t N,size_t V = ___counter_index(counter_tag<T,N>{}), size_t R  = curr_tag<T>(0,counter_tag<T,N + 1> {})>
 size_t constexpr curr_tag(int,const counter_tag<T,N>&)
 {
 	return R;
 }
 
-template<typename T, size_t R = consumer<T>(0,counter_tag<T,0> {})>
+template<typename T, size_t R = consumer<T>(0,counter_tag<T,INITIAL_VALUE> {})>
 size_t constexpr next_value()
 {
 	return R;
 }
 
-template<typename T, size_t R = curr_tag<T>(0,counter_tag<T,0> {})>
+template<typename T, size_t R = curr_tag<T>(0,counter_tag<T,INITIAL_VALUE> {})>
 size_t constexpr curr_value()
 {
 	return R;
